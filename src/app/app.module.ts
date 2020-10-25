c5541ba12d65575f0ef717818b396ac8ce97414d
Rui Wang <rui.wang.624@gmail.com>
2020-10-24 21:25:34 -0400

    initial setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './modules/shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HelpComponent } from './components/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopBarComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
